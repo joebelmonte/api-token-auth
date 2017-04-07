c8c5b8a36eadf22b3df83e96925cae72cef4d54c
yourUsername <joseph.belmonte@gmail.com>
2017-04-07 15:02:42 -0400

    Adds full feature for sign-in
    Adds curl script for sign-in as blueprint based on the library-api
    documentation. Also adds form to html for signing in.  Sign in
    uses ajax to make a request to /sign-in and stores the response in
    store.user.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const store = require('../store.js')
+
 const signUpSuccess = (data) => {
   console.log(data)
 }
@@ -8,7 +10,21 @@ const signUpFailure = (error) => {
   console.error(error)
 }
 
+const signInSuccess = (data) => {
+  console.log('Sign in successful')
+  console.log(data)
+  // store user object somehow?  You will need the token later.
+  store.data = data.user  // you want to store the user object.
+}
+
+const signInFailure = (error) => {
+  console.log('Incorrect password!')
+  console.error(error)
+}
+
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
