b7114c6e813fc513b165067188ae9239b8a9c9fb
yourUsername <joseph.belmonte@gmail.com>
2017-04-07 16:34:05 -0400

    Adding change password
    Added the ability to change password once the user is signed in.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -14,7 +14,7 @@ const signInSuccess = (data) => {
   console.log('Sign in successful')
   console.log(data)
   // store user object somehow?  You will need the token later.
-  store.data = data.user  // you want to store the user object.
+  store.user = data.user  // you want to store the user object.
 }
 
 const signInFailure = (error) => {
@@ -22,9 +22,21 @@ const signInFailure = (error) => {
   console.error(error)
 }
 
+const changePasswordSuccess = (data) => {
+  console.log('Password changed successfully.')
+  console.log(data)
+}
+
+const changePasswordFailure = (data) => {
+  console.log('Password change FAILURE.')
+  console.log(data)
+}
+
 module.exports = {
   signUpSuccess,
   signUpFailure,
   signInSuccess,
-  signInFailure
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure
 }
